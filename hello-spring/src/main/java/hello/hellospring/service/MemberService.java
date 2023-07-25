752c0646c2fa9f18168647fbda8947902fcd1ff1
Yewon So <soyw96@gmail.com>
2023-07-25 23:44:08 +0900

    [Day3] 회원 관리 예제 테스트케이스

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,22 +3,45 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 외부에서 넣어주도록 바꿔주기
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // * 회원가입
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원은 안된다!
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        // 중복 회원 검증 통과 시,
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다."); // 출력
                 });
+    }
 
-        memberRepository.save(member);
-        return member.getId();
+    // * 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // * 한명 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
