25339a8e968f9ea782ae1778392ab9b799f3ec09
Yewon So <soyw96@gmail.com>
2023-08-02 02:55:19 +0900

    [FIX] 오류 수정

--- core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextExtendsFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(...);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
 
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생")
@@ -52,7 +52,7 @@ public class ApplicationContextExtendsFindTest {
             System.out.println("key = "+key + "value = " + beansOfType.get(key));
         }
     }
-    
+
     @Test
     @DisplayName("부모 타입으로 모두 조회하기 - Object")
     void findAllBeanByObjectType() {
