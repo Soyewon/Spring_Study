10b9ab2abbf23fa782cb1299b3712b3975caeb08
Yewon So <soyw96@gmail.com>
2023-08-10 16:44:15 +0900

    [Day12] add Member Entity

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id") // pk
+    private Long id; // 회원 아이디
+
+    private String name; // 회원명
+
+    private Address address;
+
+    private List<Order> orders = new ArrayList<>();
+}
