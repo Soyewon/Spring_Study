47785ca580f51e24f081bfc2ee8c62b1ce29c386
Yewon So <soyw96@gmail.com>
2023-08-11 01:49:54 +0900

    [Day13] 연관관계 편의 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -23,10 +23,10 @@ public class Order {
     @JoinColumn(name = "member_id") // fk의 이름이 member_id가 됨
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id") // 연관관계의 주인 !
     private Delivery delivery;
 
@@ -34,4 +34,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태[ORDER, CANCEL]
+
+    // == 연관관계 메서드 ==// 양방향 시
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
