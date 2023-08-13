0d215f47be0943b8b9f7d8973b19a1b7acb3c456
Yewon So <soyw96@gmail.com>
2023-08-13 21:34:46 +0900

    [Day14] 주문 및 주문상품 엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -27,4 +27,24 @@ public class OrderItem {
 
     private int orderPrice; // 주문 당시 가격
     private int count; // 주문 당시 수량
+
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // createOrder 할 때, 이미 재고를 하나 까고 들어간다 !
+        return orderItem;
+    }
+
+    //== 비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count); // 주문 수량만큼 재고 추가해줌
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
