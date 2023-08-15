171935a0aaf94ee45c673d2b73e2c64f346e6e10
Yewon So <soyw96@gmail.com>
2023-08-15 21:26:46 +0900

    [Day16] Hello 서블릿 웰컴페이지 추가

---
+++ spring-mvc/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello") // /hello로 오면 이게 실행됨
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // 응답 메시지 찍어보기
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8"); // 문자 인코딩
+        response.getWriter().write("hello " + username);
+    }
+}
